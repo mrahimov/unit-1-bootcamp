28223cf745426c2e2c81ec63c38e3bad11a4b6ca
Murad Rahimov <rahimov.mu@gmail.com>
2018-01-01 20:00:42 -0500

    varibles

--- src/Variables.java
+++ src/Variables.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by alexandraqin on 4/14/15.
  */
@@ -5,5 +6,37 @@ public class Variables {
 
   public static void main (String args[]) {
 
+
+
+    System.out.println(isOdd(2));
+    System.out.println(isOdd(3));
+    printHelloWorld();
+
+    int [] x = {2,3,4,15,16,7,8};
+    System.out.println(greatestCommonFactor(x));
+
+  }
+  public static boolean isOdd(int x) {
+
+    if (x % 2 == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  public static void printHelloWorld() {
+    System.out.println("Hello World");
+  }
+
+  public static int greatestCommonFactor(int[] x) {
+    int gretestNumber = 0;
+    for (int i = 0; i < x.length; i++) {
+      if (x[i] > gretestNumber) {
+        gretestNumber = x[i];
+
+      }
+
+    }
+    return gretestNumber;
   }
 }
