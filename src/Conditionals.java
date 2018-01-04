5d34049ef80e2bd6e65419fa8bf5fd037a5fd221
Murad Rahimov <rahimov.mu@gmail.com>
2018-01-04 16:50:36 -0500

    finish Conditionals

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -5,11 +5,17 @@ public class Conditionals {
 
     public static void main(String args[]) {
 
-        System.out.println(isOdd(4));
-        System.out.println(isMultipleOfThree(4));
-        System.out.println(isOddAndIsMultipleOfThree(30));
-        System.out.println(isOddAndIsMultipleOfThree2(9));
-        fizzMultipleofThree(9);
+        Person person = new Person();
+        person.setName("Murad");
+        person.setCity("London");
+
+//        System.out.println(isOdd(4));
+//        System.out.println(isMultipleOfThree(4));
+//        System.out.println(isOddAndIsMultipleOfThree(30));
+//        System.out.println(isOddAndIsMultipleOfThree2(9));
+//        fizzMultipleofThree(9);
+//        System.out.println(isPersonFromLondon(person));
+//        printPersonName(person);
 
 
     }
@@ -54,4 +60,23 @@ public class Conditionals {
         }
     }
 
+    public static boolean isPersonFromLondon(Person person) {
+
+        if (person.getCity() == String.valueOf("London")) {
+            return true;
+        }
+        return false;
+    }
+    public static void printPersonName(Person person) {
+        if (person.getName().length() > 4)
+        {
+            System.out.println(person.getName());
+
+        } else if (person.getName().length() <= 4)
+        {
+            System.out.println("Name is too short.");
+        }
+    }
+
 }
+
